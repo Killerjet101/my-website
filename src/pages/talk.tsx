e2e7ba241f0391ffc13f3205985e5bde91cdc2fd
Killerjet101 <Stevenson0610@gmail.com>
2023-03-15 21:00:24 -0700

    :D

--- src/pages/talk.tsx
+++ src/pages/talk.tsx
@@ -1,4 +1,4 @@
-import useRouter from 'next/router';
+import {useRouter} from 'next/router';
 import React from 'react';
 import {toast} from 'react-hot-toast';
 import {RiPhoneLine, RiSendPlane2Line} from 'react-icons/ri';
