5a32404b986e26eddf9b486b3718112b256e8f2b
Killerjet101 <Stevenson0610@gmail.com>
2023-03-16 22:55:32 -0700

    :D

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/github',
-        destination: 'https://github.com/cnrad',
-        permanent: true,
-      },
-      {
-        source: '/twitter',
-        destination: 'https://twitter.com/notcnrad',
-        permanent: true,
-      },
-      {
-        source: '/linkedin',
-        destination: 'https://linkedin.com/in/cnrad',
-        permanent: true,
-      }
-    ]
-  },
-}
+	reactStrictMode: true,
+	swcMinify: true,
+
+	images: {
+		loader: "akamai",
+		path: "",
+	},
+};
