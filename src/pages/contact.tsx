73d1d3af5bd8fe7477a97cec17436ca7d31bb265
Killerjet101 <Stevenson0610@gmail.com>
2023-03-17 17:49:50 -0700

    :D

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MessageComponent from "../components/talk/MessageComponent";
 import ContactLink from "../components/talk/ContactLink";
-import {SiDiscord, SiTwitter, SiGmail, SiInstagram} from 'react-icons/si';
+import {SiDiscord, SiGmail, SiInstagram} from 'react-icons/si';
 import { motion } from "framer-motion";
 import TimeStatus from "../components/talk/TimeStatus";
 
@@ -17,7 +17,7 @@ const Talk = () => {
             <h1 className="text-black dark:text-white font-bold text-3xl mb-3 mt-8">Let's chat 💬</h1>
             <p className="text-gray-800 dark:text-gray-200 mb-6">
                 Have an inquiry, or want to connect? Feel free to leave a message below, get in touch via Discord,
-                or via email. Any other way maybe be a risk of not getting a response. Just an fyi.
+                or email. Any other way maybe be a risk of not getting a response. Just an fyi.
             </p>
 
             <TimeStatus />
@@ -31,13 +31,6 @@ const Talk = () => {
                         icon={<SiDiscord className="w-6 h-6 text-[#5865F2]" />}
                         link="https://discord.com/users/224207524411211777"
                         borderColor="hover:border-[#5865F2]/50"
-                    />
-
-                    <ContactLink
-                        name="@Killerjet101"
-                        icon={<SiTwitter className="w-6 h-6 text-[#1DA1F2]" />}
-                        link="https://twitter.com/Killerjet101"
-                        borderColor="hover:border-[#1DA1F2]/50"
                     />
 
                     <ContactLink
